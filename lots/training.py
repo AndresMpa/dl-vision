63c6f3dde153caa98907885ae752f4a6cc8e0532
Andres Prieto <andresprieto2703@gmail.com>
2023-12-23 00:06:03 -0500

    Lots
    Trainig by lots

---
+++ lots/training.py
@@ -0,0 +1,29 @@
+training_combinations = [
+    {"NET_ARCH": "AlexNet", "ITERATIONS": "1", "LEARNING_RATE": "0.01"},
+    {"NET_ARCH": "AlexNet", "ITERATIONS": "5", "LEARNING_RATE": "0.01"},
+    {"NET_ARCH": "AlexNet", "ITERATIONS": "10", "LEARNING_RATE": "0.01"},
+    {"NET_ARCH": "AlexNet", "ITERATIONS": "1", "LEARNING_RATE": "0.03"},
+    {"NET_ARCH": "AlexNet", "ITERATIONS": "5", "LEARNING_RATE": "0.03"},
+    {"NET_ARCH": "AlexNet", "ITERATIONS": "10", "LEARNING_RATE": "0.03"},
+
+    {"NET_ARCH": "VGG16", "ITERATIONS": "1", "LEARNING_RATE": "0.01"},
+    {"NET_ARCH": "VGG16", "ITERATIONS": "5", "LEARNING_RATE": "0.01"},
+    {"NET_ARCH": "VGG16", "ITERATIONS": "10", "LEARNING_RATE": "0.01"},
+    {"NET_ARCH": "VGG16", "ITERATIONS": "1", "LEARNING_RATE": "0.03"},
+    {"NET_ARCH": "VGG16", "ITERATIONS": "5", "LEARNING_RATE": "0.03"},
+    {"NET_ARCH": "VGG16", "ITERATIONS": "10", "LEARNING_RATE": "0.03"},
+
+    {"NET_ARCH": "VGG19", "ITERATIONS": "1", "LEARNING_RATE": "0.01"},
+    {"NET_ARCH": "VGG19", "ITERATIONS": "5", "LEARNING_RATE": "0.01"},
+    {"NET_ARCH": "VGG19", "ITERATIONS": "10", "LEARNING_RATE": "0.01"},
+    {"NET_ARCH": "VGG19", "ITERATIONS": "1", "LEARNING_RATE": "0.03"},
+    {"NET_ARCH": "VGG19", "ITERATIONS": "5", "LEARNING_RATE": "0.03"},
+    {"NET_ARCH": "VGG19", "ITERATIONS": "10", "LEARNING_RATE": "0.03"},
+
+    {"NET_ARCH": "ResNet", "ITERATIONS": "1", "LEARNING_RATE": "0.01"},
+    {"NET_ARCH": "ResNet", "ITERATIONS": "5", "LEARNING_RATE": "0.01"},
+    {"NET_ARCH": "ResNet", "ITERATIONS": "10", "LEARNING_RATE": "0.01"},
+    {"NET_ARCH": "ResNet", "ITERATIONS": "1", "LEARNING_RATE": "0.03"},
+    {"NET_ARCH": "ResNet", "ITERATIONS": "5", "LEARNING_RATE": "0.03"},
+    {"NET_ARCH": "ResNet", "ITERATIONS": "10", "LEARNING_RATE": "0.03"},
+]
